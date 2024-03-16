89300a5e8a95b2276d8a57a615d1df4687c43093
Ubuntu <ubuntu@ip-172-31-41-176.ap-south-1.compute.internal>
2024-03-16 13:22:51 +0000

    fourth commit

--- build/build.sh
+++ build/build.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
 docker rmi capstone
 docker build -t capstone .
-
+docker login -u vigneshvkr -p Vignesh@95
+if["$GITHUB_REF" == "refs/heads/dev"];
+then
+docker build -t vigneshvkr/capstone:v1 .
+docker push vigneshvkr/dev-capstone:v1
+elif["$GITHUB_REF" == "refs/heads/main"];
+then
+docker build -t vigneshvkr/capstone:v2 .
+docker push vigneshvkr/prod-capstone:v2
+else
+echo "No action. Branch is not dev or main."
+fi
--- build/deploy.sh
+++ build/deploy.sh
@@ -1,4 +1,2 @@
 #!/bin/bash
-//docker stop capstonecontainer
-//docker rm capstonecontainer
 docker run -itd -p 80:80 capstone

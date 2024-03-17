c39b7a8d05de704b622a8a5c3425288479c67bed
Ubuntu <ubuntu@ip-172-31-39-207.ap-south-1.compute.internal>
2024-03-17 12:16:48 +0000

    deploy

--- build/deploy.sh
+++ build/deploy.sh
@@ -1,2 +1,14 @@
 #!/bin/bash
 docker run -itd -p 80:80 capstone
+docker login -u vigneshvkr -p Vignesh@95
+if["$GITHUB_REF" == "refs/heads/dev"];
+then
+docker build -t vigneshvkr/capstone:v1 .
+docker push vigneshvkr/dev-capstone:v1
+elif["$GITHUB_REF" == "refs/heads/main"];
+then
+docker build -t vigneshvkr/capstone:v2 .
+docker push vigneshvkr/prod-capstone:v2
+else
+echo "No action. Branch is not dev or main."
+fi
